9635a8e71f66f97127e7d47e4234ff90884f0596
James Sgarella <Jimbo8702@gmail.com>
2023-03-29 15:20:02 -0400

    user model

---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+import { IUser } from '@/interfaces/IUser';
+import mongoose from 'mongoose';
+
+const User = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter a full name'],
+      index: true,
+    },
+
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+    },
+
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model<IUser & mongoose.Document>('User', User);
