e4dc35e394b225bedbad3a4466859e5083521446
James Sgarella <Jimbo8702@gmail.com>
2023-03-29 15:03:04 -0400

    app.ts config import and express server starter

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+import 'reflect-metadata';
+
+import config from './config';
+
+import express from 'express';
+
+//logger
+
+async function startServer() {
+  const app = express();
+
+  //load loaders
+  await require('./loaders').default({ expressApp: app });
+
+  app
+    .listen(config.port, () => {
+      //logger
+    })
+    .on('error', err => {
+      //logger
+      process.exit(1);
+    });
+}
+
+startServer();
